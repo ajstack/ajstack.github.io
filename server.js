41b3a2219d59f8a4a12fa68a1b07d59b0a729de4
Alice <a.j.stack@sbcglobal.net>
2018-12-07 20:02:03 -0600

    hopefully client/server is connected

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
