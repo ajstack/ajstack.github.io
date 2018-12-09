58144f0a6e3aa4cc34332e59528cc55ac188754e
Alice <a.j.stack@sbcglobal.net>
2018-12-09 14:08:28 -0600

    starting over

--- client/src/components/contactTitle/contactTitle.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./contactTitle.css";
-
-const ContactTitle = props => <h1 className="Contact-title">{props.children}</h1>;
-
-export default ContactTitle;
--- client/src/components/portfolioTitle/portfolioTitle.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./portfolioTitle.css";
-
-const PortfolioTitle = props => <h1 className="portfolio-title">{props.children}</h1>;
-
-export default PortfolioTitle;
