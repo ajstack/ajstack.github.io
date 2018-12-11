9491d2bc7ee39a9c485a5c2f81f1ee0e20805bc7
Alice <a.j.stack@sbcglobal.net>
2018-12-11 13:11:15 -0600

    starting over again

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-// Define middleware here
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-// Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
