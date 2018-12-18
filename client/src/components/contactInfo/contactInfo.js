93026d0e4779b78a9841c948a01f8656ec388906
Alice <a.j.stack@sbcglobal.net>
2018-12-18 11:47:39 -0600

    adding things back in

---
+++ client/src/components/contactInfo/contactInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./contactInfo.css";
+
+const ContactInfo = () => (
+    <div className="contact-info">
+        <h2>Test</h2>
+    </div>
+)
+
+export default ContactInfo;
---
+++ client/src/pages/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.css";
+import HomepageTitle from "../../components/homepageTitle"
+
+const Homepage = () => (
+    <div className="homepage">
+        <HomepageTitle>Homepage!</HomepageTitle>
+    </div>
+
+);
+
+export default Homepage;
