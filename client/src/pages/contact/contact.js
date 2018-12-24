376750ed1dc13f30e1134e2c2c7870319413a20b
Alice <a.j.stack@sbcglobal.net>
2018-12-24 14:44:26 -0600

    changed name

--- client/src/pages/contact/contact.js
+++ client/src/pages/contact/contact.js
@@ -8,11 +8,10 @@ const Contact = () => (
         <ContactTitle>Contact!</ContactTitle>
         <ContactInfoCard />
         <div className="credit-info">LinkedIn Icon made by 
-            <a href="https://www.freepik.com/" title="Freepik">Freepik</a> and
-            Github icon made by
+            <a href="https://www.freepik.com/" title="Freepik">Freepik</a> and Github icon made by
             <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from 
             <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> and is licensed by 
-            <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+            <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a>
         </div>
     </div>
 
