64d6a70cd388f9f2734cfe50b0dced7aa4781464
Alice <a.j.stack@sbcglobal.net>
2018-12-24 18:16:28 -0600

    added 'still working' message

--- client/src/components/aboutMeCard/aboutMeCard.js
+++ client/src/components/aboutMeCard/aboutMeCard.js
@@ -8,6 +8,9 @@ const AboutMeCard = props => (
       Ambitious Full Stack Developer. Has three years of experience in retail, gaining effective communication skills . 
       Has an eye for detail that translates in to organized and readable code. 
       UT Coding Bootcamp graduate with a passion for improvement.
+      <br />
+      <br />
+      Pardon the construction, still busy building a portfolio I can be proud of!
     </p>
   </div>
 )
