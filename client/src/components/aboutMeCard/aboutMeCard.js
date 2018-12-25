515ce247373e59d7cddc1dd5b2a3fe87b87a9162
Alice <a.j.stack@sbcglobal.net>
2018-12-24 18:11:42 -0600

    working on homepage

---
+++ client/src/components/aboutMeCard/aboutMeCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./aboutMeCard.css";
+
+const AboutMeCard = props => (
+  <div className="about-me-card">
+    <h1 className="about-me-title">About Me</h1>
+    <p className="about-me-content">
+      Ambitious Full Stack Developer. Has three years of experience in retail, gaining effective communication skills . 
+      Has an eye for detail that translates in to organized and readable code. 
+      UT Coding Bootcamp graduate with a passion for improvement.
+    </p>
+  </div>
+)
+
+export default AboutMeCard;
--- client/src/pages/homepage/homepage.js
+++ client/src/pages/homepage/homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./homepage.css";
 import HomepageTitle from "../../components/homepageTitle";
+import AboutMeCard from "../../components/aboutMeCard";
 
 const Homepage = () => (
   <div className="homepage">
     <HomepageTitle>Homepage!</HomepageTitle>
+    <AboutMeCard />
   </div>
 
 );
