2b85b6109368b3db4d1e161d9ee50e83f995dc51
Alice <a.j.stack@sbcglobal.net>
2019-01-04 18:24:31 -0600

    added css

--- client/src/pages/homepage/homepage.js
+++ client/src/pages/homepage/homepage.js
@@ -2,13 +2,20 @@ import React from "react";
 import "./homepage.css";
 import HomepageTitle from "../../components/homepageTitle";
 import AboutMeCard from "../../components/aboutMeCard";
+import Portfolio from "../portfolio";
+
 
 const Homepage = () => (
-  <div className="homepage">
-    <HomepageTitle>Alicia Stack</HomepageTitle>
-    <AboutMeCard />
-  </div>
+  <div className="container">
+    <div className="homepage">
+      <HomepageTitle>Alicia Stack</HomepageTitle>
+      <AboutMeCard />
+    </div>
 
+    <div className="portfolio-section">
+      <Portfolio />
+    </div>
+  </div>
 );
 
 export default Homepage;
