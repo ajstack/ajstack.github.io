2877a33bce0e6186f1169f6dbf662d9b6258bc62
Alice <a.j.stack@sbcglobal.net>
2019-01-04 19:14:26 -0600

    changing fonts

--- client/src/components/portfolioTitle/portfolioTitle.js
+++ client/src/components/portfolioTitle/portfolioTitle.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../assets/fonts.css";
 import "./portfolioTitle.css";
 
 const PortfolioTitle = props => <h1 className="portfolio-title">{props.children}</h1>;
